1ce9b793cbe64672228ab57b6738dd5e2ae7d72e
bob6664569 <johan@trilogik.net>
2018-04-29 14:15:05 +0200

    Initial package

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # blockfolio-api
-API Client for Blockfolio
+
+[![Faire un don en Bitcoin](https://fr.cryptobadges.io/badge/micro/1LhMTZWBnRq6NTwWegYKdMUAiH9LrWEvyd)](https://fr.cryptobadges.io/donate/1LhMTZWBnRq6NTwWegYKdMUAiH9LrWEvyd)
+
+Non-official Node.JS API Client for Blockfolio
+
+Install
+--
+```sh
+npm install blockfolio-api --save
+```
+
+Usage
+--
+```javascript
+const blockfolio = require("blockfolio-api")("BLOCKFOLIO_DEVICE_TOKEN");
+
+blockfolio.getPositions((err, positions) => {
+
+    if (err) throw(err);
+
+    console.log(positions);
+});
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+/*jslint node: true, nomen: true, regexp: true, plusplus: true */
+(function () {
+
+    'use strict';
+
+    /**
+     *
+     * Blockfolio API Endpoints
+     *
+     * /register/{e}?platform={platform}
+     * /lastprice/{e}/{base}-{tokenSymbol}?locale={_}&use_alias=true
+     * /coinlist_v6
+     * /get_all_positions/{token}?fiat_currency={r}&locale={_}&use_alias=true
+     * /exchangelist_v2/{base}-{tokenSymbol}
+     * /currency
+     * /news?{e}
+     * /updateUserExchange?token={n}&coin={tokenSymbol}&base={base}&exchange={g(t)}
+     * /news_sources
+     * /updateUserCoinToken?token={t}{e}
+     * /add_position_v2/{token}/1/{tokenSymbol}/{base}/{n}/{price}/{quantity}/{r}/{watch}/?platform={platform}&note={o}&use_alias=true
+     * /updateToken/{e}/{t}
+     * /edit_position_v3/{token}/{id}/{tokenSymbol}/{n}/{price}/{quantity}/{r}/{watch}/?platform={platform}&note={o}
+     * /remove_position/{token}/{id}
+     * /remove_alert/{token}/{e}?use_alias=true
+     * /remove_all_positions/{token}/{base}-{tokenSymbol}?use_alias=true
+     * /marketdetails_v2/{token}/{o}/{base}-{tokenSymbol}?fiat_currency={r}&locale={_}&use_alias=true
+     * /orderbook/{token}/{base}-{tokenSymbol}?locale={_}&use_alias=true
+     * /get_alerts/{token}?coin={e}&base={t}&locale={_}&use_alias=true
+     * /add_alert/{token}/{id}/{tokenSymbol}/{base}/{n}/{above}/{below}/{frequency}?platform={platform}&use_alias=true
+     * /edit_alert/{token}/{id}/{n}/{above}/{below}/{frequency}?use_alias=true
+     * /pause/{token}/{e}?use_alias=true
+     * /start/{token}/{e}?use_alias=true
+     * /start_all/{token}/{tokenSymbol}/{base}?use_alias=true
+     * /get_positions_v2/{token}/{base}-{tokenSymbol}/?fiat_currency={r}&locale={_}&use_alias=true
+     * /get_combined_position/{token}/{base}-{tokenSymbol}?fiat_currency={r}&locale={_}&use_alias=true
+     * /get_coin_summary/{token}/{base}-{tokenSymbol}?fiat_currency={r}&locale={_}&use_alias=true
+     * /candlestick/{r}/{u}/{base}-{tokenSymbol}/{t}?locale={_}&fiat_currency={r}&use_alias=true
+     * /portfolio_v2/{token}/{period}?fiat_currency={r}&locale={_}&use_alias=true
+     * /version?platform={platform}
+     * /change_log?platform={platform}
+     * /system_status?platform={platform}
+     * /error_log?platform={platform}
+     * /add_ico?token={token}&id={e}
+     * /remove_ico?token={token}&id={e}
+     * /ico
+     * /announcements
+     *
+     * Tools
+     * periods: {"1D":"day","1W":"week","1M":"month","3M":"3month","6M":"6month","1Y":"year",ALL:"all"}
+     */
+
+    const request = require('request');
+
+    const API_URL =  "https://api-v0.blockfolio.com/rest/";
+    const magic = "edtopjhgn2345piuty89whqejfiobh89-2q453"; // Common token to pseudo-authenticate API Client
+
+
+})();
